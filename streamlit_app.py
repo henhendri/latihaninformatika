6246e50d696df2d34cf75ec08877b81da5945acb
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-04-24 11:39:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.write(
     "Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python"
 )
     
-    st.image("view/young.png width=200")
+st.image("view/young.png width=200")
