659f96c410655a3a484321799cba2d8fa45bb151
Mr.Hendri <68919425+henhendri@users.noreply.github.com>
2025-05-04 13:06:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,24 @@ st.write(
 )
     
 st.image("view/young.png")
+
+
+st.header (":mailbox: Get in touch with Me!")
+st.title(f"Mail Message", anchor=False)
+#st.page == "Contact":
+st.write("Feel free to contact us with any questions or inquiries.")
+
+contact_form_html = """
+    <form action="https://formsubmit.co/your@email.com" method="POST">
+         <input type="text" name="name" placeholder="Your name" required>
+         <input type="email" name="email" placeholder="Your email" required>
+         <textarea name="message" placeholder="Your message here"></textarea>
+         <button type="submit">Send</button>
+    </form>
+"""
+
+st.markdown(contact_form_html, unsafe_allow_html=True)
+
+# You can add other contact information below the form
+st.write("Email: contact@example.com")
+st.write("Phone: 123-456-7890")
